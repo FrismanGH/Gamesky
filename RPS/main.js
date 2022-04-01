9287a96055a43329eb71b40f787a62fd334ae6c3
FrismanGH <spidermansebik@seznam.cz>
2022-04-01 10:53:51 +0200

    AAAAAAAAAAAAAAAA

--- RPS/main.js
+++ RPS/main.js
@@ -1,4 +1,10 @@
-const computerChoice = document.getElementById("computer-choice");
-const userChoice = document.getElementById("user-choice");
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
 const resultDisplay = document.getElementById("result");
-const possibleChoice = document.querySelectorAll("button");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+
+ possibleChoices.forEach(possibleChoice => possibleChoice.AddEventListener("click", (e) => {
+     userChoice = e.target.id;
+     userChoiceDisplay.innerHTML = userChoice;
+ }))
